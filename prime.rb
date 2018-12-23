ace25c665ad57559a5596af489c9dd13044bb0a5
michael hussain <smhussain_1@yahoo.com>
2018-12-23 21:14:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
 def prime?(n)
- (2..n-1).none? {|divisor| n % divisor ==0}
+ #(2..n-1).none? {|divisor| n % divisor ==0}
  #(2..n-(-1)).none? {|divisor| n % divisor ==0}
+ 
+ if n <= 0
+   false
+ elsif n % 2 == 0 || n % 3 == 0 || n % 5 == 0
+   false
+ else
+   true
+ end
 end
